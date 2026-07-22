0de549dbd17f4e09d9b826034cfc977abda07419
GB220 <gbedoyah@unal.edu.co>
2026-07-21 19:02:41 -0500

    Sol

--- homework/pregunta_01.py
+++ homework/pregunta_01.py
@@ -3,6 +3,9 @@
 Escriba el codigo que ejecute la accion solicitada.
 """
 
+import os
+import pandas as pd
+import matplotlib.pyplot as plt
 
 def pregunta_01():
     """
@@ -35,3 +38,118 @@ def pregunta_01():
     * Su código debe crear la carpeta `docs` si no existe.
 
     """
+    def loadData():
+        df = pd.read_csv("files/input/shipping-data.csv")
+        return df
+
+    def create_visual_for_shipping_per_warehouse(df):
+        df = df.copy()
+        plt.figure()
+
+        counts = df.Warehouse_block.value_counts()
+        counts.plot.bar(
+            title="Shipping per Warehouse Block",
+            xlabel="Warehouse Block",
+            ylabel="Record Count",
+            color="tab:blue",
+            fontsize=8,
+        )
+        plt.gca().spines["top"].set_visible(False)
+        plt.gca().spines["right"].set_visible(False)
+        plt.savefig("docs/shipping_per_warehouse.png")
+
+    def create_visual_for_mode_of_shipment(df):
+        df = df.copy()
+        plt.figure()
+
+        counts = df.Mode_of_Shipment.value_counts()
+        counts.plot.pie(
+            title="Mode of Shipment",
+            wedgeprops=dict(width=0.35),
+            ylabel="",
+            color=["tab:blue", "tab:orange", "tab:green"],
+        )
+        plt.savefig("docs/mode_of_shipment.png")
+
+    def create_visual_for_average_customer_rating(df):
+        df = df.copy()
+        plt.figure()
+        df = (
+            df[["Mode_of_Shipment", "Customer_rating"]]
+            .groupby("Mode_of_Shipment")
+            .describe()
+        )
+        df.columns = df.columns.droplevel()
+        df = df[["mean", "min", "max"]]
+        plt.barh(
+            y=df.index.values,
+            width=df["max"].values - 1,
+            left=df["min"].values,
+            height=0.9,
+            color="lightgrey",
+            alpha=0.8,
+        )
+
+        colors = [
+            "tab:green" if value >= 3.0 else "tab:orange" for value in df["mean"].values
+        ]
+
+        plt.barh(
+            y=df.index.values,
+            width=df["mean"].values - 1,
+            left=df["min"].values,
+            color=colors,
+            height=0.5,
+            alpha=1.0,
+        )
+
+        plt.title("Average Customer Rating by Mode of Shipment")
+        plt.gca().spines["left"].set_color("grey")
+        plt.gca().spines["bottom"].set_color("grey")
+        plt.gca().spines["top"].set_visible(False)
+        plt.gca().spines["right"].set_visible(False)
+
+        plt.savefig("docs/average_customer_rating.png")
+
+    def create_visual_for_weight_distribution(df):
+        df = df.copy()
+        plt.figure()
+        df.Weight_in_gms.plot.hist(
+            title="Shipped Weight Distribution",
+            color="tab:orange",
+            edgecolor="white",
+        )
+
+        plt.gca().spines["top"].set_visible(False)
+        plt.gca().spines["right"].set_visible(False)
+        plt.savefig("docs/weight_distribution.png")
+
+    def crear_index_html():
+        contenido = """<!DOCTYPE html>
+        <html>
+        <body>
+            <h1>Shipping Dashboard Example</h1>
+            <div style="width:45%;float:left">
+            <img src="shipping_per_warehouse.png" alt="Fig 1">
+            <img src="mode_of_shipment.png" alt="Fig 2">
+            </div>
+            <div style="width:45%;float:left">
+            <img src="average_customer_rating.png" alt="Fig 3">
+            <img src="weight_distribution.png" alt="Fig 4">
+            </div>
+        </body>
+        </html>"""
+
+        with open("docs/index.html", "w", encoding="utf-8") as f:
+            f.write(contenido)
+
+    df = loadData()
+    os.makedirs("docs", exist_ok=True)
+    create_visual_for_shipping_per_warehouse(df)
+    create_visual_for_mode_of_shipment(df)
+    create_visual_for_average_customer_rating(df)
+    create_visual_for_weight_distribution(df)
+    crear_index_html()
+
+if __name__ == "__main__":
+    pregunta_01()
